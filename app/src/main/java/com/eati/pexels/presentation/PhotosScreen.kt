801156dac1eebee377f30c445ea31b8c4f26875d
Bruno Velazquez <42704038+BrunoVelazquez@users.noreply.github.com>
2023-03-03 12:55:26 -0300

    First

--- app/src/main/java/com/eati/pexels/presentation/PhotosScreen.kt
+++ app/src/main/java/com/eati/pexels/presentation/PhotosScreen.kt
@@ -1,10 +1,21 @@
 package com.eati.pexels.presentation
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.eati.pexels.domain.Photo
-
 
 @Composable
 fun PhotosScreen(viewModel: PhotosViewModel) {
@@ -15,10 +26,102 @@ fun PhotosScreen(viewModel: PhotosViewModel) {
 
 @Composable
 fun Photos(results: List<Photo>, updateResults: (String) -> Unit) {
-
     LaunchedEffect(Unit) {
         updateResults("architecture")
     }
+    var listLastIndex = results.size - 1
+    val mapPhotographerPhoto : MutableMap<String, MutableList<String>> = mutableMapOf()
+    for(i in 0..listLastIndex) {
+        var photographer: String = results[i].photographer
+        var listPhotos: MutableList<String>? = mapPhotographerPhoto.get(photographer)
+        if(listPhotos == null) {
+            var newList: MutableList<String> = mutableListOf(results[i].photoUrl)
+            mapPhotographerPhoto.put(photographer, newList)
+        } else {
+            listPhotos.add(results[i].photoUrl)
+            mapPhotographerPhoto[photographer] = listPhotos
+        }
+    }
 
-    Text(text = results.toString())
+    val listUniquePhotographers : MutableList<UniquePhotographer> = mutableListOf()
+    mapPhotographerPhoto.keys.forEach {
+        listUniquePhotographers.add(UniquePhotographer(it, mapPhotographerPhoto.get(it)))
+    }
+
+    Column(modifier = Modifier.padding(8.dp)) {
+        PhotographersRow(list = listUniquePhotographers)
+    }
+
+}
+
+data class UniquePhotographer(val photographerName: String, val photos: MutableList<String>?)
+
+@Composable
+fun PhotographerThing(
+    photographerName: String,
+    list: List<UniquePhotographer>,
+    showPhotos:(List<UniquePhotographer>) -> Unit,
+    modifier: Modifier = Modifier
+){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Button(onClick = {
+            showPhotos()
+        },
+            shape = RoundedCornerShape(100.dp),
+            modifier = Modifier
+                .size(100.dp)
+        ) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(50.dp)
+                )
+                Text(
+                    text = photographerName,
+                    style = MaterialTheme.typography.h2,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 8.sp,
+                    color = Color.White
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun PhotographersRow(
+    list: List<UniquePhotographer>,
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items(list) { item ->
+            PhotographerThing(
+                photographerName = item.photographerName,
+                list
+            )
+        }
+    }
+}
+
+@Composable
+fun showPhotos(
+    photographer: UniquePhotographer
+) {
+    LazyRow(
+        modifier = Modifier.fillMaxHeight(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        items(photographer.photos) {item ->
+
+        }
+    }
 }
